786276ebd06199607fa028ec0fe19eb693cf60f5
qhuydtvt <qhuydtvt@gmail.com>
2016-10-19 13:59:26 +0700

    Added POST API

--- app.py
+++ app.py
@@ -1,20 +1,27 @@
 import json
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
-post = {"title": "Today I felt tired", "content" : "Today, as a programmer, I felt no hopes, no dreams and no lives"}
-post2 = {"title": "Today I met a girl", "content" : "Today I met a girl, she is pretty, she loves ice cream and movies"}
+post = {"title": "I felt tired", "content" : "Today, as a programmer, I felt no hopes, no dreams and no lives"}
+post2 = {"title": "I met a girl", "content" : "Today I met a girl, she is pretty, she loves ice cream and movies"}
 
 posts = [
     post,
     post2
 ]
 
-@app.route('/')
+@app.route('/', methods=["GET", "POST"])
 def posts_api():
-    return json.dumps(posts)
-
+    if request.method == "GET":
+        return json.dumps(posts)
+    elif request.method == "POST":
+        request_body = request.get_json()
+        title = request_body["title"]
+        content = request_body["content"]
+        post = {"title": title, "content": content}
+        posts.append(post)
+        return json.dumps({"code" : 1, "message": "Post added"})
 
 if __name__ == '__main__':
     app.run()
